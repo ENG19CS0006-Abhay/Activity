7a5491eff4e497329be5f24371b8447da30f7c48
ENG19CS0006-Abhay <76053889+ENG19CS0006-Abhay@users.noreply.github.com>
2021-02-03 13:14:19 +0530

    Create Activity3.c

---
+++ Activity3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float h,b,d;
+    printf("Enter the values of h,b,d");
+    scanf("%f%f%f",&h,&b,&d);
+    float vol=1/3*((h*d*b)+d/b);
+    printf("The volume is %f",vol);
+
+    return 0;
+}
